87f3a365915a91636059f758c9cbe45b7ed0de74
FarhanZubir <faehan19@gmail.com>
2022-04-22 02:35:20 +0800

    mastercommit

--- comment.php
+++ comment.php
@@ -7,12 +7,15 @@
     <title>Document</title>
 </head>
 <body>
-<?php// This is a comment
-// echo 
+<?php
+// This is a comment 
+// echo  
 "X equals $x"; 
-$x += 10; // Increment 
-$x by 10 echo $x;
+$x += 10; 
+// Increment $x by 10 
+echo $x;
 /* This is a section of multiline comments
-that will not be interpreted */?>
+that will not be interpreted */
+?>
 </body>
 </html>
